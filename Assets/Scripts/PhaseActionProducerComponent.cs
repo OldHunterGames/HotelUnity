a333621c8e8ec6861ed6353a1e7cd7c686072332
Nf Sq <nfsqsqsq@gmail.com>
2015-07-18 01:35:21 +0300

    Couple of stubs for the time system.

---
+++ Assets/Scripts/PhaseActionProducerComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhaseActionProducerComponent : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ShortActionProducerComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShortActionProducerComponent : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
