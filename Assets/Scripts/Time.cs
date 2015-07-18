7ce56ff0cd699f64b299ff4c67b52512803801d2
Denkun <koty998@gmail.com>
2015-07-18 16:59:20 +0300

    Add time text on all locations

---
+++ Assets/Scripts/Time.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Time : MonoBehaviour {
+	Text TimeText;
+
+	string[] TimeName = {"Утро", "День", "Вечер", "Ночь"}; 
+	static int Now = 0;
+
+	void OnLevelWasLoaded(int level){
+		UpdateTime();
+	}
+
+	public void FaseOut(){
+		Now++;
+		Now %= 4;
+		UpdateTime();
+	}
+
+	void UpdateTime(){
+		TimeText = GameObject.Find ("TimeText").GetComponent<Text> ();
+		TimeText.text = TimeName[Now];
+	}
+}
