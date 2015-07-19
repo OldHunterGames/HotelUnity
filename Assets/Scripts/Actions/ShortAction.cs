692bc222833461cd878faa53a6a0c2ba5e358435
Nf Sq <nfsqsqsq@gmail.com>
2015-07-20 02:58:37 +0300

    Short and Phase actions base classes.

---
+++ Assets/Scripts/Actions/ShortAction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ShortAction {
+
+	public GameObject actionSource;
+	public GameObject actionTarget;
+
+	public abstract void ExecuteShortAction();
+}
