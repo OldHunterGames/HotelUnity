e334f0d874e2194ea3fd04e133abff49d6fc4dd1
Nf Sq <nfsqsqsq@gmail.com>
2015-07-20 03:05:05 +0300

    Actions producers.

---
+++ Assets/Scripts/WalkerPhaseActionsProducer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkerPhaseActionsProducer : PhaseActionsProducer {
+		
+	public override PhaseAction ProducePhaseAction() {
+		var action = new SleepPhaseAction ();
+		action.actionSource = gameObject;
+		action.actionTarget = gameObject;
+
+		return action;
+	}
+	
+	public override void OnPhaseStart () {}
+}
