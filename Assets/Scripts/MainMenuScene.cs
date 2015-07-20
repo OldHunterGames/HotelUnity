70037a1fd9e89cd0c1d483b13216589d93362e49
Nf Sq <nfsqsqsq@gmail.com>
2015-07-20 03:05:56 +0300

    GUI helpers.

---
+++ Assets/Scripts/MainMenuScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScene : MonoBehaviour {
+	
+	void Start () {
+		GameObject.DontDestroyOnLoad (GameObject.Find ("Characters"));
+		Application.LoadLevel ("Lobby");
+	}
+}
