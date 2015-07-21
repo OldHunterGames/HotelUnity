9d9cfa6b6081429b88d2b49d1a71cb33776954b0
Roman <bwrentau@gmail.com>
2015-07-21 21:57:08 +0300

    36. TimeMachine Refactoring
    Create separate methods for phaseActionsProducers and
    shortActionsProducers handling.
    Implement method reloadCharactersData().

--- Assets/Scripts/TimeMachine.cs
+++ Assets/Scripts/TimeMachine.cs
@@ -6,30 +6,13 @@ public class TimeMachine {
 
 	public static readonly TimeMachine Instance = new TimeMachine();
 
+	private List<PhaseActionsProducer> phaseActionsProducers = new List<PhaseActionsProducer> ();
+	private List<ShortActionsProducer> shortActionsProducers = new List<ShortActionsProducer> ();
+	private List<PhaseAction> phaseActions = new List<PhaseAction>();
+	private List<ShortAction> shortActions = new List<ShortAction>();
+
 	public int ExecuteShortActions() {
-		var characters = GameObject.Find ("Characters");
-
-		Debug.Assert (characters != null, "Scene should contains 'Characters' node.");
-
-		var shortActions = new List<ShortAction>();
-
-		for (int childIndex = 0; childIndex < characters.transform.childCount; childIndex++) {
-			var character = characters.transform.GetChild(childIndex);
-
-			var producerComponent = character.GetComponent<ShortActionsProducerComponent>();
-
-			if (producerComponent) {
-				var producer = producerComponent.actionsProducer;
-
-				if (producer) {
-					var action = producer.ProduceShortAction();
-
-					if (action != null) {
-						shortActions.Add(action);
-					}
-				}
-			}
-		}
+		reloadCharactersData ();
 
 		foreach (var shortAction in shortActions) {
 			shortAction.ExecuteShortAction();
@@ -39,62 +22,9 @@ public class TimeMachine {
 	}
 
 	public void ExecutePhaseActions() {
+		reloadCharactersData ();
+
 		while (ExecuteShortActions() > 0) {}
-
-		var characters = GameObject.Find ("Characters");
-		
-		Debug.Assert (characters != null, "Scene should contains 'Characters' node.");
-		
-		var phaseActions = new List<PhaseAction>();
-
-		for (int childIndex = 0; childIndex < characters.transform.childCount; childIndex++) {
-			var character = characters.transform.GetChild(childIndex);
-			
-			var producerComponent = character.GetComponent<PhaseActionsProducerComponent>();
-			
-			if (producerComponent) {
-				var producer = producerComponent.actionsProducer;
-				
-				if (producer) {
-					var action = producer.ProducePhaseAction();
-					
-					if (action != null) {
-						phaseActions.Add(action);
-					}
-				}
-			}
-		}
-		
-		foreach (var phaseAction in phaseActions) {
-			phaseAction.ExecutePhaseAction();
-		}
-
-		var phaseActionsProducers = new List<PhaseActionsProducer> ();
-		var shortActionsProducers = new List<ShortActionsProducer> ();
-
-		for (int childIndex = 0; childIndex < characters.transform.childCount; childIndex++) {
-			var character = characters.transform.GetChild(childIndex);
-			
-			var phaseActionsProducerComponent = character.GetComponent<PhaseActionsProducerComponent>();
-			
-			if (phaseActionsProducerComponent) {
-				var producer = phaseActionsProducerComponent.actionsProducer;
-				
-				if (producer) {
-					phaseActionsProducers.Add(producer);
-				}
-			}
-
-			var shortActionsProducerComponent = character.GetComponent<ShortActionsProducerComponent>();
-			
-			if (shortActionsProducerComponent) {
-				var producer = shortActionsProducerComponent.actionsProducer;
-				
-				if (producer) {
-					shortActionsProducers.Add(producer);
-				}
-			}
-		}
 
 		foreach (var producer in phaseActionsProducers) {
 			producer.OnPhaseStart();
@@ -102,6 +32,50 @@ public class TimeMachine {
 
 		foreach (var producer in shortActionsProducers) {
 			producer.OnPhaseStart();
+		}
+	}
+
+	private void reloadCharactersData() {
+		phaseActionsProducers.Clear ();
+		shortActionsProducers.Clear ();
+		phaseActions.Clear ();
+		shortActions.Clear ();
+
+		var characters = GameObject.Find ("Characters");
+    	Debug.Assert (characters != null, "Scene should contains 'Characters' node.");
+
+		for (int childIndex = 0; childIndex < characters.transform.childCount; childIndex++) {
+			var character = characters.transform.GetChild (childIndex);
+			processShortActionsProducers(character);
+			processPhaseActionsProducers(character);
+		}
+	}
+
+	private void processShortActionsProducers(Transform character) {
+		var shortActionsProducerComponent = character.GetComponent<ShortActionsProducerComponent> ();
+		if (shortActionsProducerComponent) {
+			var producer = shortActionsProducerComponent.actionsProducer;
+			if (producer) {
+				shortActionsProducers.Add (producer);
+				var action = producer.ProduceShortAction();
+				if (action != null) {
+					shortActions.Add(action);
+				}
+			}
+		}
+	}
+
+	private void processPhaseActionsProducers(Transform character) {
+		var phaseActionsProducerComponent = character.GetComponent<PhaseActionsProducerComponent> ();
+		if (phaseActionsProducerComponent) {
+			var producer = phaseActionsProducerComponent.actionsProducer;
+			if (producer) {
+				phaseActionsProducers.Add (producer);
+				var action = producer.ProducePhaseAction();
+				if (action != null) {
+					phaseActions.Add(action);
+				}
+			}
 		}
 	}
 }
