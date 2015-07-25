b2dbb6e6ae67987927b4db536ac74f64b5bf2a43
Nf Sq <nfsqsqsq@gmail.com>
2015-07-25 22:36:35 +0300

    Hunger character component.

---
+++ Assets/Scripts/CharactersComponents/HungerCharacterComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HungerCharacterComponent : MonoBehaviour, IPhaseEventsListener {
+
+	public int hunger = 0;
+
+	public void OnPhaseFinish() {
+		hunger += 1;
+	}
+}
