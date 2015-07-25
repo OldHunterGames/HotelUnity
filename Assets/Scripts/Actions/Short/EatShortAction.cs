86054a9add430fb5d2980ecce94cb76c192027a1
Nf Sq <nfsqsqsq@gmail.com>
2015-07-25 23:19:13 +0300

    'Eater' bot.

---
+++ Assets/Scripts/Actions/Short/EatShortAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EatShortAction : ShortAction {
+		
+	override public void ExecuteShortAction() {
+		var component = actionSource.GetComponent<HungerCharacterComponent> ();
+
+		Debug.Assert (component != null, "Source object should attach HungerCharacterComponent.");
+
+		if (component.hunger > 0) {
+			component.hunger -= 1;
+		}
+		
+		Debug.Log (string.Format ("Short action '{0}'. '{1}' eats.", GetType(), actionSource.name));
+	}
+}
---
+++ Assets/Scripts/Producers/Short/EaterShortActionsProducer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EaterShortActionsProducer : WalkerShortActionsProducer {
+
+	public override ShortAction ProduceShortAction () {
+		var componentHunger = gameObject.GetComponent<HungerCharacterComponent> ();
+		Debug.Assert (componentHunger != null, "Object should attach HungerCharacterComponent.");
+
+		var componentLocation = gameObject.GetComponent<CharacterLocationComponent> ();
+		Debug.Assert (componentLocation != null, "Object should attach CharacterLocationComponent.");
+
+		if (componentHunger.hunger > 0 && componentLocation.sublocation.Equals("DiningRoom")) {
+			var action = new EatShortAction();
+			action.actionSource = gameObject;
+
+			return action;
+		} else {
+			return base.ProduceShortAction ();
+		}
+	}
+
+	public override void OnPhaseFinish () {
+		var component = gameObject.GetComponent<HungerCharacterComponent> ();
+
+		Debug.Assert (component != null, "Object should attach HungerCharacterComponent.");
+
+		if (component.hunger == 5) {
+			targetSublocation = "DiningRoom";
+		} else {
+			base.OnPhaseFinish ();
+		}
+	}
+}
