5c433ed3155fdafdde4e1dac4c56c7d3fa161c94
Denkun <koty998@gmail.com>
2015-07-27 01:21:25 +0300

    #34 Add bot that satisfies his needs

---
+++ Assets/Scripts/Actions/Phase/NeederSleepPhaseAction.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeederSleepPhaseAction : PhaseAction {
+	
+	override public void ExecutePhaseAction() {
+		var component = actionSource.GetComponent<NeedsCharacterComponent> ();
+		
+		Debug.Assert (component != null, "Source object should attach NeedsCharacterComponent.");
+		
+		if (component.Freshness < 15) {
+			component.Freshness += 5;
+			if (component.Freshness > 15) component.Freshness = 15;
+		}
+	}
+}
---
+++ Assets/Scripts/Producers/Short/NeederShortActionProducer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeederShortActionProducer : WalkerShortActionsProducer {
+	
+	public override ShortAction ProduceShortAction () {
+		var componentNeeds = gameObject.GetComponent<NeedsCharacterComponent> ();
+		Debug.Assert (componentNeeds != null, "Object should attach NeedsCharacterComponent.");
+		
+		var componentLocation = gameObject.GetComponent<CharacterLocationComponent> ();
+		Debug.Assert (componentLocation != null, "Object should attach CharacterLocationComponent.");
+		
+		if (componentNeeds.Hunger > 0 && componentLocation.sublocation.Equals("DiningRoom")) {
+			var action = new NeederEatShortAction();
+			action.actionSource = gameObject;
+
+			return action;
+		} else {
+			return base.ProduceShortAction ();
+		}
+	}
+	
+	public override void OnPhaseFinish () {
+		var component = gameObject.GetComponent<NeedsCharacterComponent> ();
+		
+		Debug.Assert (component != null, "Object should attach NeedsCharacterComponent.");
+
+		if (component.Hunger == 5) {
+			targetSublocation = "DiningRoom";
+		} else if (component.NeedForSleep == 5) {
+			targetSublocation = "RoomInside";
+		} else {
+			base.OnPhaseFinish ();
+		}
+	}
+}
