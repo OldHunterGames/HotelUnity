82420f678d355562f7439860a485838a7f0be7c3
Roman <bwrentau@gmail.com>
2015-07-28 00:59:03 +0300

    #39. Added basic features for location as object.
    Added basic features for location as object.
    
    Main idea: Now each location can produce some actions on Phase finish.
    Such as increasing the number of meals etc.
    
    Modified folders structure.

--- Assets/Scripts/MainMenuScene.cs
+++ Assets/Scripts/MainMenuScene.cs
@@ -5,6 +5,7 @@ public class MainMenuScene : MonoBehaviour {
 	
 	void Start () {
 		GameObject.DontDestroyOnLoad (GameObject.Find ("Characters"));
+		GameObject.DontDestroyOnLoad (GameObject.Find ("Locations"));
 		Application.LoadLevel ("Lobby");
 	}
 }
---
+++ Assets/Scripts/Producers/Phase/LocationPhaseActionsProducer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocationPhaseActionsProducer : PhaseActionsProducer {
+		
+	public override PhaseAction ProducePhaseAction() {
+		var action = new IdlePhaseAction ();
+		action.actionSource = gameObject;
+		action.actionTarget = gameObject;
+
+		return action;
+	}
+
+	public override void OnPhaseFinish () {}
+}
