d4f0fc9a8a1aefcc9543a73cf84d3c0367e54d78
Denkun <koty998@gmail.com>
2015-07-31 14:00:03 +0300

    #32 remove constructors from character wants component

--- Assets/Scripts/CharactersComponents/WantsCharacterComponent.cs
+++ Assets/Scripts/CharactersComponents/WantsCharacterComponent.cs
@@ -3,44 +3,15 @@ using System.Collections;
 
 public class WantsCharacterComponent : MonoBehaviour {
 
-	public int Altruism;
-	public int Affinity;
-	public int Authority;
-	public int Hedonism;
-	public int Greed;
-	public int Communication;
-	public int Curiosity;
-	public int Order;
-	public int Entertainment;
-	public int Creativity;
-	public int Ambition;
-
-	public WantsCharacterComponent(){
-		Altruism = 3;
-		Affinity = 3;
-		Authority = 3;
-		Hedonism = 3;
-		Greed = 3;
-		Communication = 3;
-		Curiosity = 3;
-		Order = 3;
-		Entertainment = 3;
-		Creativity = 3;
-		Ambition = 3;
-	}
-
-	public WantsCharacterComponent(int al, int af, int au, int he, int gr, int co, 
-	                               int cu, int or, int en, int cr, int am){
-		Altruism = al;
-		Affinity = af;
-		Authority = au;
-		Hedonism = he;
-		Greed = gr;
-		Communication = co;
-		Curiosity = cu;
-		Order = or;
-		Entertainment = en;
-		Creativity = cr;
-		Ambition = am;
-	}
+	public int Altruism = 3;
+	public int Affinity = 3;
+	public int Authority = 3;
+	public int Hedonism = 3;
+	public int Greed = 3;
+	public int Communication = 3;
+	public int Curiosity = 3;
+	public int Order = 3;
+	public int Entertainment = 3;
+	public int Creativity = 3;
+	public int Ambition = 3;
 }
