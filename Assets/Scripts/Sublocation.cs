b7e71d0ba6d22a142bafdbbb83cdd0ef024fb210
Nf Sq <nfsqsqsq@gmail.com>
2015-08-05 03:06:52 +0300

    Sublocation helper component.

---
+++ Assets/Scripts/Sublocation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sublocation : MonoBehaviour {
+
+	public ShortActionFabric[] ShortActionFabrics {
+		get {
+			return gameObject.GetComponentsInChildren<ShortActionFabric>();
+		}
+	}
+
+	public PhaseActionFabric[] PhaseActionFabrics {
+		get {
+			return gameObject.GetComponentsInChildren<PhaseActionFabric>();
+		}
+	}
+}
