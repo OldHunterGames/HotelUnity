c160bf185630c4dfde1787cd306eceb7cad2164a
Nf Sq <nfsqsqsq@gmail.com>
2015-08-05 03:11:38 +0300

    Character helper component.

---
+++ Assets/Scripts/Components/Characters/Character.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public Sublocation Sublocation {
+		get {
+			return gameObject.transform.parent.transform.parent.GetComponent<Sublocation>();
+		}
+	}
+}
