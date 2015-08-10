6faad008898f6529793b210c8f8e858289edee1b
Roman <bwrentau@gmail.com>
2015-08-10 16:15:25 +0300

    #45: Added Sleep Character Component
    Added Sleep Character Component

---
+++ Assets/Scripts/Components/Characters/SleepCharacterComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+
+public class SleepCharacterComponent : MonoBehaviour, IPhaseEventsListener {
+
+	private const int minFreshness = 0;
+	private const int maxFreshness = 15;
+
+	[SerializeField]
+	private int _freshness = maxFreshness;
+
+	public int freshness {
+		get {
+			return _freshness;
+		}
+		set {
+			_freshness = RangeValidator.validate(value, minFreshness, maxFreshness);
+		}
+	}
+	
+	public void OnPhaseFinish() {
+		--freshness;
+	}
+}
