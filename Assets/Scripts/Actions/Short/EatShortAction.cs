65371d247ee98526ec44ce83cf085b0015b6cec4
Roman <bwrentau@gmail.com>
2015-08-10 10:36:52 +0300

    No issue: Added support FoodDetailsComponent to EatShortAction.
    Added support FoodDetailsComponent to EatShortAction.

--- Assets/Scripts/Actions/Short/EatShortAction.cs
+++ Assets/Scripts/Actions/Short/EatShortAction.cs
@@ -5,13 +5,15 @@ public class EatShortAction : ShortAction {
 		
 	override public void ExecuteShortAction() {
 		var component = actionTarget.GetComponent<HungerCharacterComponent> ();
-
 		Debug.Assert (component != null, "Source object should attach HungerCharacterComponent.");
+		var foodDetails = actionSource.GetComponent<FoodDetailsComponent> ();
+		Debug.Assert (component != null, "Source object should attach FoodDetailsComponent.");
 
-		if (component.hunger > 0) {
-			component.hunger -= 1;
+		component.hunger -= foodDetails.hungerRestore;
+		if (component.hunger < 0) {
+			component.hunger = 0;
 		}
 		
-		Debug.Log (string.Format ("Short action '{0}'. '{1}' eats.", GetType(), actionTarget.name));
+		Debug.Log (string.Format ("Short action '{0}'. '{1}' eats {2}.", GetType(), actionTarget.name, foodDetails.foodName));
 	}
 }
--- Assets/Scripts/Fabrics/Short/EatShortActionFabric.cs
+++ Assets/Scripts/Fabrics/Short/EatShortActionFabric.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class EatShortActionFabric : ShortActionFabric {
 
-	public string foodName;
-
 	override public ShortAction CreateShortAction(GameObject target) {
 		var action = new EatShortAction ();
 		action.actionSource = gameObject;
@@ -14,6 +12,9 @@ public class EatShortActionFabric : ShortActionFabric {
 
 	public override string caption {
 		get {
+			var foodInfo = gameObject.GetComponent<FoodDetailsComponent>();
+			string foodName = foodInfo.foodName.Length > 0 ? foodInfo.foodName : gameObject.name;
+
 			return string.Format("Съесть {0}", foodName);
 		}
 	}
