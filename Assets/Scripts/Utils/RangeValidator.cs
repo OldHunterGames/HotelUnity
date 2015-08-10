91f4e3e7070df6649719af1d71a0e7e73adcc280
Roman <bwrentau@gmail.com>
2015-08-10 16:09:48 +0300

    #45: Added RangeValidator
    Added RangeValidator.
    Validator chack value and:
    - returns minValue if the value is less than minValue .
    - returns maxValue if the value is greater than maxValue.
    - returns value if its in range.

---
+++ Assets/Scripts/Utils/RangeValidator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Utils {
+
+	public class RangeValidator {
+
+		public static int validate(int value, int minValue, int maxValue) {
+			int result = 0;
+
+			if (value > maxValue) {
+				result = maxValue;
+			} else if (value < minValue) {
+				result = minValue;
+			} else {
+				result = value;
+			}
+
+			return result;
+		}
+	}
+}
