d58e878fe225f071ea850e23464b96843beef511
Roman <bwrentau@gmail.com>
2015-08-10 10:35:11 +0300

    No issue: Added FoodDetailsComponent.
    Added FoodDetailsComponent.

---
+++ Assets/Scripts/Components/Objects/FoodDetailsComponent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodDetailsComponent : MonoBehaviour {
+
+	public string foodName;
+	public string foodDescription;
+	public int hungerRestore;
+}
