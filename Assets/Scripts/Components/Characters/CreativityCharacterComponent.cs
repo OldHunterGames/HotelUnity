a0c62f41affaf184e3a081afab743516bd7be77e
Roman <bwrentau@gmail.com>
2015-08-10 16:15:01 +0300

    #45: Added Creativity Character Component
    Added Creativity Character Component

---
+++ Assets/Scripts/Components/Characters/CreativityCharacterComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+
+public class CreativityCharacterComponent : MonoBehaviour, IPhaseEventsListener {
+	
+	private const int minCreativity = 0;
+	private const int maxCreativity = 15;
+
+	[SerializeField]
+	private int _creativity = minCreativity;
+	
+	public int creativity {
+		get {
+			return _creativity;
+		}
+		set {
+			_creativity = RangeValidator.validate(value, minCreativity, maxCreativity);
+		}
+	}
+	
+	public void OnPhaseFinish() {
+		--creativity;
+	}
+}
