d750660598857a9164b13ac3178a1f6081603d9c
Roman <bwrentau@gmail.com>
2015-08-11 21:10:27 +0300

    #54: Implemented Dancing
    Implemented Dancing

---
+++ Assets/Scripts/Actions/Phase/DancingPhaseAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DancingPhaseAction : PhaseAction {
+
+	override public void ExecuteAction() {
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' is '{2}'.", GetType (), actionTarget.name, actionSource.name));
+		
+		var action = new SatisfyCreativityAction ();
+		action.actionSource = actionSource;
+		action.actionTarget = actionTarget;
+		action.ExecuteAction ();
+	}
+}
---
+++ Assets/Scripts/Fabrics/Phase/DancingPhaseActionFabric.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DancingPhaseActionFabric : PhaseActionFabric {
+
+	public string title;
+	
+	override public PhaseAction CreatePhaseAction(GameObject target) {
+		var action = new DancingPhaseAction ();
+		action.actionSource = gameObject;
+		action.actionTarget = target;
+		
+		return action;
+	}
+	
+	public override string caption {
+		get {
+			return title;
+		}
+	}
+}
