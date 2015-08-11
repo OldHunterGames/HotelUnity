b074df146bab7af3a3e5639d1495fcdabd5275d7
Roman <bwrentau@gmail.com>
2015-08-11 21:17:59 +0300

    #56: Implemented painting on easel
    Implemented painting on easel
    Added Easel prefab.

---
+++ Assets/Scripts/Actions/Phase/EaselPhaseAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EaselPhaseAction : PhaseAction {
+	
+	override public void ExecuteAction() {
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' painting on '{2}'.", GetType (), actionTarget.name, actionSource.name));
+		
+		var action = new SatisfyCreativityAction ();
+		action.actionSource = actionSource;
+		action.actionTarget = actionTarget;
+		action.ExecuteAction ();
+	}
+}
---
+++ Assets/Scripts/Fabrics/Phase/EaselPhaseActionFabric.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EaselPhaseActionFabric : PhaseActionFabric {
+	
+	public string title;
+	
+	override public PhaseAction CreatePhaseAction(GameObject target) {
+		var action = new EaselPhaseAction ();
+		action.actionSource = gameObject;
+		action.actionTarget = target;
+		
+		return action;
+	}
+	
+	public override string caption {
+		get {
+			return title;
+		}
+	}
+}
