378e3a28d989831e15d6add9c47830bf40aa2c93
Roman <bwrentau@gmail.com>
2015-08-11 19:25:30 +0300

    #49: Implemented reading fiction books.
    Implemented reading fiction books.

---
+++ Assets/Scripts/Actions/Phase/FictionBookcasePhaseAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FictionBookcasePhaseAction : PhaseAction {
+
+	override public void ExecuteAction() {
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' reading book from '{2}'.", GetType(), actionTarget.name, actionSource.name));
+		
+		var action = new SatisfyEntertainmentAction ();
+		action.actionSource = actionSource;
+		action.actionTarget = actionTarget;
+		action.ExecuteAction ();
+	}
+}
---
+++ Assets/Scripts/Fabrics/Phase/FictionBookcasePhaseActionFabric.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FictionBookcasePhaseActionFabric : PhaseActionFabric {
+	
+	public string title;
+	
+	override public PhaseAction CreatePhaseAction(GameObject target) {
+		var action = new FictionBookcasePhaseAction ();
+		action.actionSource = gameObject;
+		action.actionTarget = target;
+		
+		return action;
+	}
+	
+	public override string caption {
+		get {
+			return title;
+		}
+	}
+}
