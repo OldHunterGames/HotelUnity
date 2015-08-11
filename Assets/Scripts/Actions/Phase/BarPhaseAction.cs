ac8b217a953f6d83e2d9a86203787c4ce7e665d9
Roman <bwrentau@gmail.com>
2015-08-11 20:14:29 +0300

    #50. Implement Bar object.
    Implement Bar object.
    Add prefab for Bar object.

---
+++ Assets/Scripts/Actions/Phase/BarPhaseAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarPhaseAction : PhaseAction {
+
+	override public void ExecuteAction() {
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' drinks in '{2}'.", GetType(), actionTarget.name, actionSource.name));
+		
+		var action = new SatisfyEntertainmentAction ();
+		action.actionSource = actionSource;
+		action.actionTarget = actionTarget;
+		action.ExecuteAction ();
+	}
+}
---
+++ Assets/Scripts/Fabrics/Phase/BarPhaseActionFabric.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarPhaseActionFabric : PhaseActionFabric {
+
+	public string title;
+	
+	override public PhaseAction CreatePhaseAction(GameObject target) {
+		var action = new BarPhaseAction ();
+		action.actionSource = gameObject;
+		action.actionTarget = target;
+		
+		return action;
+	}
+	
+	public override string caption {
+		get {
+			return title;
+		}
+	}
+}
