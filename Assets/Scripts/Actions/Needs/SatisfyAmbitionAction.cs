50b56c85040a4811e4a0b9b477b45d99f19f350c
Roman <bwrentau@gmail.com>
2015-08-11 20:35:55 +0300

    #52: Implemented Gym object
    Implemented Gym object
    Added SatisfyAmbitionAction.
    Added AmbitionObjectDetails.

---
+++ Assets/Scripts/Actions/Needs/SatisfyAmbitionAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SatisfyAmbitionAction : Action {
+
+	public override void ExecuteAction() {
+		var needComponent = actionTarget.GetComponent<AmbitionCharacterComponent> ();
+		Debug.Assert (needComponent != null, "Source object should attach AmbitionCharacterComponent.");
+		var needDetails = actionSource.GetComponent<AmbitionObjectDetails> ();
+		Debug.Assert (needDetails != null, "Source object should attach AmbitionObjectDetails.");
+		
+		needComponent.ambition -= needDetails.needSatisfyRate;
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' satisfy his Ambition need via '{2}'.", GetType(), actionTarget.name, actionSource.name));
+	}
+}
---
+++ Assets/Scripts/Actions/Phase/GymPhaseAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GymPhaseAction : PhaseAction {
+
+	override public void ExecuteAction() {
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' training in the '{2}'.", GetType(), actionTarget.name, actionSource.name));
+		
+		var action = new SatisfyAmbitionAction ();
+		action.actionSource = actionSource;
+		action.actionTarget = actionTarget;
+		action.ExecuteAction ();
+	}
+}
