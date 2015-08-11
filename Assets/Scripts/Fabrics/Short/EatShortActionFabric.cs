3394af2658bfc72869808e98fe144612084acc44
Roman <bwrentau@gmail.com>
2015-08-11 17:27:05 +0300

    No issue: Fixed prefabs.
    Fixed prefabs.

--- Assets/Scripts/Fabrics/Short/EatShortActionFabric.cs
+++ Assets/Scripts/Fabrics/Short/EatShortActionFabric.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EatShortActionFabric : ShortActionFabric {
 
-	override public ShortAction CreateShortAction(GameObject target) {
+	public override ShortAction CreateShortAction(GameObject target) {
 		var action = new EatShortAction ();
 		action.actionSource = gameObject;
 		action.actionTarget = target;
