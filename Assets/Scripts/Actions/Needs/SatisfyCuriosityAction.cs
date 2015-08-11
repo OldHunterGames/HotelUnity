85031c7f69d794b89099de282945d5d79e2bc9a1
Roman <bwrentau@gmail.com>
2015-08-11 19:24:47 +0300

    #48: Implemented reading science books.
    Implemented reading science books.
    Create prefab for scienceBookcase.
    Added SatisfyCuriosityAction.
    Added CuriosityObjectDetails.

---
+++ Assets/Scripts/Actions/Needs/SatisfyCuriosityAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SatisfyCuriosityAction : Action {
+
+	public override void ExecuteAction() {
+		var needComponent = actionTarget.GetComponent<CuriosityCharacterComponent> ();
+		Debug.Assert (needComponent != null, "Source object should attach CuriosityCharacterComponent.");
+		var needDetails = actionSource.GetComponent<CuriosityObjectDetails> ();
+		Debug.Assert (needDetails != null, "Source object should attach CuriosityObjectDetails.");
+		
+		needComponent.curiosity -= needDetails.needSatisfyRate;
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' satisfy his Curiosity need via '{2}'.", GetType(), actionTarget.name, actionSource.name));
+	}
+}
---
+++ Assets/Scripts/Actions/Phase/ScienceBookcasePhaseAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScienceBookcasePhaseAction : PhaseAction {
+
+	override public void ExecuteAction() {
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' reading book from '{2}'.", GetType(), actionTarget.name, actionSource.name));
+		
+		var action = new SatisfyCuriosityAction ();
+		action.actionSource = actionSource;
+		action.actionTarget = actionTarget;
+		action.ExecuteAction ();
+	}
+}
