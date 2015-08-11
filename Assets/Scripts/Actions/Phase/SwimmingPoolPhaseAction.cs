4287d9ab5ba78902a7dc9326f8fc8604e325ae70
Roman <bwrentau@gmail.com>
2015-08-11 20:23:49 +0300

    #51: Implement SwimmingPool
    Implement SwimmingPool

---
+++ Assets/Scripts/Actions/Phase/SwimmingPoolPhaseAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwimmingPoolPhaseAction : PhaseAction {
+
+	override public void ExecuteAction() {
+		Debug.Log (string.Format ("Phase action '{0}'. '{1}' is swimming in the '{2}'.", GetType(), actionTarget.name, actionSource.name));
+		
+		var action = new SatisfyHedonismAction ();
+		action.actionSource = actionSource;
+		action.actionTarget = actionTarget;
+		action.ExecuteAction ();
+	}
+}
